9066851063810b5cc05cd0ef179b73efdaf66bb4
John Higgins <johnmatthiggins@gmail.com>
2022-09-01 23:30:17 -0700

    ▓▓▓▓▓▓  ▓▓▓▓▓▓

--- git_ascii.sh
+++ git_ascii.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 for i in $(seq 1 $(cat $1 | wc -l))
 do
-    OUTPUT="$(head -n $i $1 | tail -n 1)"
-    echo $OUTPUT
+    echo $RANDOM >> something.txt
+    git add .;
+    git commit -m "$(head -n $i $1 | tail -n 1)";
 done
+
+rm something.txt
+git add .;
+git commit -m " ";
