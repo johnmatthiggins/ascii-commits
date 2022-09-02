647fa0cd74c1c76b56b6fe45d4795306fe4ad1d3
John Higgins <johnmatthiggins@gmail.com>
2022-09-01 23:31:59 -0700

    Fixed upside down issue

--- git_ascii.sh
+++ git_ascii.sh
@@ -3,9 +3,9 @@ for i in $(seq 1 $(cat $1 | wc -l))
 do
     echo $RANDOM >> something.txt
     git add .;
-    git commit -m "$(head -n $i $1 | tail -n 1)";
+    git commit -m "$(tail -n $i $1 | head -n 1)";
 done
 
 rm something.txt
 git add .;
-git commit -m " ";
+git commit -m "ALIGNMENT COMMIT";
